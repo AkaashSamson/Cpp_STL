1bb02e608152826e0a89a25cece5be056bf2cb86
AkaashSamson <akaashsam22@gmail.com>
2024-09-12 07:53:20 +0530

    set

--- main.cpp
+++ main.cpp
@@ -192,6 +192,30 @@ void explainPriorityQueue(){
     //push - logn, top - O(1), pop - logn
 }
 
+void explainSet(){
+    set<int> st;
+    st.insert(1); // {1}
+    st.emplace(2); // {1, 2}
+    st.emplace(4); // {1, 2, 4}
+    st.insert(3); // {1, 2, 3, 4}
+    st.insert(3); // {1, 2, 3, 4} - No duplicates
+  
+    auto it = st.find(3); // Returns the iterator to the element if found else returns st.end()
+
+    st.erase(3); // {1, 2, 4}
+
+    int cnt = st.count(3); // Returns the count of the element in the set - 0
+
+    auto it2 = st.find(2);
+    auto it3 = st.find(4);
+    st.erase(it2, it3); // {1}
+
+    auto it = st.lower_bound(2); // Returns the iterator to the element which is equal to or just greater than the given element
+    auto it = st.upper_bound(2); // Returns the iterator to the element which is just greater than the given element
+
+    // rest functions same as vector
+}
+
 int main(){
     explainList();
     return 0;

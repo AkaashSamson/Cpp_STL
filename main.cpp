39098d3a13de2e189ed22334b9c1ec1f5da96bef
AkaashSamson <akaashsam22@gmail.com>
2024-09-12 07:34:58 +0530

    deque

--- main.cpp
+++ main.cpp
@@ -114,6 +114,27 @@ void explainList(){
     //begin(), end(), rbegin(), rend(), size(), clear(), insert(), erase(), swap(), remove(), reverse()
 }
 
+void explainDeque(){
+    deque<int> dq;
+    dq.push_back(1);
+    dq.emplace_back(2);
+
+    dq.push_front(3);
+    dq.emplace_front(4);
+    
+    dq.pop_back();
+    dq.pop_front();
+    dq.back();
+    dq.front();
+    for(auto it: dq){
+        cout << it << " ";
+    }
+    cout << endl;
+
+    //rest functions same as vector
+    //begin(), end(), rbegin(), rend(), size(), clear(), insert(), erase(), swap(), pop_front(), pop_back()
+}
+
 int main(){
     explainList();
     return 0;
